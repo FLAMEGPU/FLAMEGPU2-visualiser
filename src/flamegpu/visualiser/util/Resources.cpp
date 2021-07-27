5d9f36a7d657ad25b5df23cca4e12a25003f7e7e
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2021-07-27 13:32:21 +0100

    Update visualisation tmp cache directory to match main lib. (#72)
    * Update visualisation tmp cache directory to match main lib.
    
    * Adjust temp directory to match flamegpu post embedded version PR
    
    Co-authored-by: Peter Heywood <peethwd@gmail.com>

--- src/flamegpu/visualiser/util/Resources.cpp
+++ src/flamegpu/visualiser/util/Resources.cpp
@@ -39,12 +39,12 @@ namespace {
         static path result;
         if (result.empty()) {
             path tmp =  std::getenv("FLAMEGPU2_TMP_DIR") ? std::getenv("FLAMEGPU2_TMP_DIR") : temp_directory_path();
-            // Create the $tmp/fgpu2/vis folder hierarchy
+            // Create the $tmp/flamegpu/vis folder hierarchy
             if (!::exists(tmp) && !create_directory(tmp)) {
                 THROW InvalidFilePath("Directory '%s' does not exist and cannot be created by visualisation.", tmp.generic_string().c_str());
             }
             if (!std::getenv("FLAMEGPU2_TMP_DIR")) {
-                tmp /= "fgpu2";
+                tmp /= "flamegpu";
                 if (!::exists(tmp)) {
                     create_directory(tmp);
                 }
