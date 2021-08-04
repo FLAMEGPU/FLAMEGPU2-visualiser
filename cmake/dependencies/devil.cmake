23f0667fba73ce5a7a8e1d021b687b9a20764589
Peter Heywood <peethwd@gmail.com>
2021-08-04 11:05:31 +0100

    On windows, do not try to find libraries before dowloading them
    There are issues with CMakes find package with multi-config generators / msvc, where a release lib will be found but not debug, which can break the build (apparently).

--- cmake/dependencies/devil.cmake
+++ cmake/dependencies/devil.cmake
@@ -9,23 +9,20 @@ if(UNIX)
                         "e.g. sudo apt install libdevil-dev")
     endif()
 elseif(WIN32)
-    # On windows, if DevIL cannot be found, it is downloaded and CMake is told how to find it (via config mode)
-    find_package(DevIL QUIET NO_MODULE)
-    if(NOT DEVIL_FOUND)
-        # Declare source properties
-        FetchContent_Declare(
-            devil
-            URL "http://downloads.sourceforge.net/openil/DevIL-Windows-SDK-1.8.0.zip"
-        )
-        FetchContent_GetProperties(devil)
-        if(NOT devil_POPULATED)
-            # Download content
-            FetchContent_Populate(devil)
-            # Create a Cmake configuraiton file for devil (the download is not cmake aware)
-            set(DevIL_DIR ${devil_SOURCE_DIR})
-            configure_file(${CMAKE_CURRENT_LIST_DIR}/devil-config.cmake.in ${devil_SOURCE_DIR}/devil-config.cmake @ONLY)
-            # Find it again. Erroring if it cannot be found.
-            find_package(DevIL REQUIRED NO_MODULE)
-        endif()
+    # On windows, always download manually. There are issues with find_package and multi-config generators where a release library will be found, but no debug library, which can break things.
+    # Declare source properties
+    FetchContent_Declare(
+        devil
+        URL "http://downloads.sourceforge.net/openil/DevIL-Windows-SDK-1.8.0.zip"
+    )
+    FetchContent_GetProperties(devil)
+    if(NOT devil_POPULATED)
+        # Download content
+        FetchContent_Populate(devil)
+        # Create a Cmake configuraiton file for devil (the download is not cmake aware)
+        set(DevIL_DIR ${devil_SOURCE_DIR})
+        configure_file(${CMAKE_CURRENT_LIST_DIR}/devil-config.cmake.in ${devil_SOURCE_DIR}/devil-config.cmake @ONLY)
+        # Find it again. Erroring if it cannot be found.
+        find_package(DevIL REQUIRED NO_MODULE)
     endif()
 endif()
--- cmake/dependencies/glew.cmake
+++ cmake/dependencies/glew.cmake
@@ -9,23 +9,20 @@ if(UNIX)
                             "e.g. sudo apt install libglew-dev")
     endif ()
 elseif(WIN32)
-    # On windows, if GLEW cannot be found, it is downloaded and CMake is told how to find it (via config mode)
-    find_package(GLEW QUIET)
-    if(NOT GLEW_FOUND)
-        # Declare source properties
-        FetchContent_Declare(
-            glew
-            URL "https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip"
-        )
-        FetchContent_GetProperties(glew)
-        if(NOT glew_POPULATED)
-            # Download content
-            FetchContent_Populate(glew)
-            # Create a Cmake configuraiton file for glew (the download is not cmake aware)
-            set(GLEW_DIR ${glew_SOURCE_DIR})
-            configure_file(${CMAKE_CURRENT_LIST_DIR}/glew-config.cmake.in ${glew_SOURCE_DIR}/glew-config.cmake @ONLY)
-            # Find it again. Erroring if it cannot be found.
-            find_package(GLEW REQUIRED)
-        endif()
+    # On windows, always download manually. There are issues with find_package and multi-config generators where a release library will be found, but no debug library, which can break things.
+    # Declare source properties
+    FetchContent_Declare(
+        glew
+        URL "https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip"
+    )
+    FetchContent_GetProperties(glew)
+    if(NOT glew_POPULATED)
+        # Download content
+        FetchContent_Populate(glew)
+        # Create a Cmake configuraiton file for glew (the download is not cmake aware)
+        set(GLEW_DIR ${glew_SOURCE_DIR})
+        configure_file(${CMAKE_CURRENT_LIST_DIR}/glew-config.cmake.in ${glew_SOURCE_DIR}/glew-config.cmake @ONLY)
+        # Find it again. Erroring if it cannot be found.
+        find_package(GLEW REQUIRED)
     endif()
 endif()
