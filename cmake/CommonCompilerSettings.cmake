5e2e280101409bc52ea316b1356679cb0452d748
Peter Heywood <peethwd@gmail.com>
2021-09-03 17:58:56 +0100

    Update CMake minimum version to 3.18
    Includes some of the refactored CMake from FLAMEGPU/FLAMEGPU2
    
    Drops CI to use GCC-8 to avoid custom CMake warning with CUDA 11.0 and GCC 9.
    
    Do not enable override warnings, as cpplint does not approve of final override

---
+++ cmake/CommonCompilerSettings.cmake
@@ -0,0 +1,67 @@
+# Define a function which can be used to set common compiler options for a target
+# We do not want to force these options on end users (although they should be used ideally), hence not just public properties on the library target
+# Function to suppress compiler warnings for a given target
+function(CommonCompilerSettings)
+    # Parse the expected arguments, prefixing variables.
+    cmake_parse_arguments(
+        CCS
+        ""
+        "TARGET"
+        ""
+        ${ARGN}
+    )
+
+    # Ensure that a target has been passed, and that it is a valid target.
+    if(NOT CCS_TARGET)
+        message( FATAL_ERROR "function(CommonCompilerSettings): 'TARGET' argument required")
+    elseif(NOT TARGET ${CCS_TARGET} )
+        message( FATAL_ERROR "function(CommonCompilerSettings): TARGET '${CCS_TARGET}' is not a valid target")
+    endif()
+
+    # Add device debugging symbols to device builds of CUDA objects
+    target_compile_options(${CCS_TARGET} PRIVATE "$<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Debug>>:-G>")
+    # Ensure DEBUG and _DEBUG are defined for Debug builds
+    target_compile_definitions(${CCS_TARGET} PRIVATE $<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Debug>>:DEBUG>)
+    target_compile_definitions(${CCS_TARGET} PRIVATE $<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Debug>>:_DEBUG>)
+    # Enable -lineinfo for Release builds, for improved profiling output.
+    target_compile_options(${CCS_TARGET} PRIVATE "$<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Release>>:-lineinfo>")
+
+    # Set an NVCC flag which allows host constexpr to be used on the device.
+    target_compile_options(${CCS_TARGET} PRIVATE "$<$<COMPILE_LANGUAGE:CUDA>:--expt-relaxed-constexpr>")
+
+    # Prevent windows.h from defining max and min.
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        target_compile_definitions(${CCS_TARGET} PRIVATE NOMINMAX)
+    endif()
+
+    # Pass the SEATBELTS macro, which when set to off/0 (for non debug builds) removes expensive operations.
+    if (SEATBELTS)
+        # If on, all build configs have  seatbelts
+        target_compile_definitions(${CCS_TARGET} PRIVATE SEATBELTS=1)
+    else()
+        # Id off, debug builds have seatbelts, non debug builds do not.
+        target_compile_definitions(${CCS_TARGET} PRIVATE $<IF:$<CONFIG:Debug>,SEATBELTS=1,SEATBELTS=0>)
+    endif()
+
+    # MSVC handling of SYSTEM for external includes.
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 19.10)
+        # These flags don't currently have any effect on how CMake passes system-private includes to msvc (VS 2017+)
+        set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "/external:I")
+        set(CMAKE_INCLUDE_SYSTEM_FLAG_CUDA "/external:I")
+        # VS 2017+
+        target_compile_options(${CCS_TARGET} PRIVATE "$<$<COMPILE_LANGUAGE:CXX>:/experimental:external>")
+    endif()
+
+    # Enable parallel compilation
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        target_compile_options(${CCS_TARGET} PRIVATE "$<$<COMPILE_LANGUAGE:CUDA>:SHELL:-Xcompiler /MP>")
+        target_compile_options(${CCS_TARGET} PRIVATE "$<$<COMPILE_LANGUAGE:C,CXX>:/MP>")
+    endif()
+
+    # If CUDA 11.2+, can build multiple architectures in parallel. 
+    # Note this will be multiplicative against the number of threads launched for parallel cmake build, which may lead to processes being killed, or excessive memory being consumed.
+    if(CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL "11.2" AND USE_NVCC_THREADS AND DEFINED NVCC_THREADS AND NVCC_THREADS GREATER_EQUAL 0)
+        target_compile_options(${CCS_TARGET} PRIVATE "$<$<COMPILE_LANGUAGE:CUDA>:SHELL:--threads ${NVCC_THREADS}>")
+    endif()
+
+endfunction()
