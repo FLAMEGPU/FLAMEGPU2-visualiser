FLAMEGPU/FLAMEGPU2-visualiser

commit 3966b665391811322741a7369081f6ac3aa4d55e

Add support for providing agent location as float2 or float3
Requires matching main repo commit.

src/flamegpu/visualiser/Visualiser.cpp
@@ -7,6 +7,7 @@
 
 #include "flamegpu/visualiser/util/cuda.h"
 #include "flamegpu/visualiser/util/fonts.h"
+#include "flamegpu/visualiser/shader/PositionFunction.h"
 #include "flamegpu/visualiser/shader/DirectionFunction.h"
 #include "flamegpu/visualiser/shader/ScaleFunction.h"
 #include "flamegpu/visualiser/shader/lights/LightsBuffer.h"
@@ -56,6 +57,7 @@ Visualiser::RenderInfo::RenderInfo(const AgentStateConfig& vc,
             custom_texture_buffers.emplace(c.first, std::make_pair(c.second, nullptr));
         }
         // Select the corresponding shader
+        PositionFunction pf(_core_tex_buffers);
         DirectionFunction df(_core_tex_buffers);
         ScaleFunction sf(_core_tex_buffers);
         if (!vc.color_shader_src.empty()) {
@@ -64,20 +66,20 @@ Visualiser::RenderInfo::RenderInfo(const AgentStateConfig& vc,
                 vc.model_path,
                 *reinterpret_cast<const glm::vec3*>(vc.model_scale),
                 std::make_shared<Shaders>(
-                    "resources/instanced_default_Tcolor_Tdir_Tscale.vert",
+                    "resources/instanced_default_Tcolor_Tpos_Tdir_Tscale.vert",
                     "resources/material_flat_Tcolor.frag",
                     "",
-                    sf.getSrc() + df.getSrc() + vc.color_shader_src));
+                    pf.getSrc() + df.getSrc() + sf.getSrc() + vc.color_shader_src));
         } else if (vc.model_texture) {
             // Entity has texture
             entity = std::make_shared<Entity>(
                 vc.model_path,
                 *reinterpret_cast<const glm::vec3*>(vc.model_scale),
                 std::make_shared<Shaders>(
-                    "resources/instanced_default_Tdir_Tscale.vert",
+                    "resources/instanced_default_Tpos_Tdir_Tscale.vert",
                     "resources/material_phong.frag",
                     "",
-                    sf.getSrc() + df.getSrc()),
+                    pf.getSrc() + df.getSrc() + sf.getSrc()),
                 Texture2D::load(vc.model_texture));
         } else {
             // Entity does not have a texture
@@ -85,10 +87,10 @@ Visualiser::RenderInfo::RenderInfo(const AgentStateConfig& vc,
                 vc.model_path,
                 *reinterpret_cast<const glm::vec3*>(vc.model_scale),
                 std::make_shared<Shaders>(
-                    "resources/instanced_default_Tdir_Tscale.vert",
+                    "resources/instanced_default_Tpos_Tdir_Tscale.vert",
                     "resources/material_flat.frag",
                     "",
-                    sf.getSrc() + df.getSrc()));
+                    pf.getSrc() + df.getSrc() + sf.getSrc()));
             entity->setMaterial(glm::vec3(0.1f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.7f));
         }
 }
@@ -488,10 +490,10 @@ void Visualiser::renderAgentStates() {
                 shader_vec->removeTextureUniform(samplerName.c_str());
                 CUDATextureBuffer<float> *old_tb = tb;
                 // Alloc new buffs (this needs to occur in render thread!)
-                tb = mallocGLInteropTextureBuffer<float>(newSize, 1);
+                tb = mallocGLInteropTextureBuffer<float>(newSize * TexBufferConfig::SamplerElements(_tb.first), 1);
                 // Copy any old data to the buffer
                 if (old_tb && tb && old_tb->d_mappedPointer && tb->d_mappedPointer && as.dataSize)
-                    _cudaMemcpyDeviceToDevice(tb->d_mappedPointer, old_tb->d_mappedPointer, as.dataSize * sizeof(float));
+                    _cudaMemcpyDeviceToDevice(tb->d_mappedPointer, old_tb->d_mappedPointer, as.dataSize * sizeof(float) * TexBufferConfig::SamplerElements(_tb.first));
                 // Bind texture name to texture unit
                 GL_CALL(glActiveTexture(GL_TEXTURE0 + as.tex_unit_offset + tui));
                 GL_CALL(glBindTexture(GL_TEXTURE_BUFFER, tb->glTexName));
@@ -509,10 +511,10 @@ void Visualiser::renderAgentStates() {
                 shader_vec->removeTextureUniform(_tb.second.first.nameInShader.c_str());
                 CUDATextureBuffer<float>* old_tb = tb;
                 // Alloc new buffs (this needs to occur in other thread!!!)
-                tb = mallocGLInteropTextureBuffer<float>(newSize, 1);
+                tb = mallocGLInteropTextureBuffer<float>(newSize * TexBufferConfig::SamplerElements(_tb.first), 1);
                 // Copy any old data to the buffer
                 if (old_tb && tb && old_tb->d_mappedPointer && tb->d_mappedPointer && as.dataSize)
-                    _cudaMemcpyDeviceToDevice(tb->d_mappedPointer, old_tb->d_mappedPointer, as.dataSize * sizeof(float));
+                    _cudaMemcpyDeviceToDevice(tb->d_mappedPointer, old_tb->d_mappedPointer, as.dataSize * sizeof(float) * TexBufferConfig::SamplerElements(_tb.first));
                 // Bind texture name to texture unit
                 GL_CALL(glActiveTexture(GL_TEXTURE0 + as.tex_unit_offset + tui));
                 GL_CALL(glBindTexture(GL_TEXTURE_BUFFER, tb->glTexName));
@@ -583,13 +585,13 @@ void Visualiser::updateAgentStateBuffer(const std::string &agent_name, const std
         for (const auto &_ext_tb : ext_core_tex_buffers) {
             auto &ext_tb = _ext_tb.second;
             auto &int_tb = as.core_texture_buffers.at(_ext_tb.first);
-            visassert(_cudaMemcpyDeviceToDevice(int_tb->d_mappedPointer, ext_tb.t_d_ptr, as.dataSize * sizeof(float)));
+            visassert(_cudaMemcpyDeviceToDevice(int_tb->d_mappedPointer, ext_tb.t_d_ptr, as.dataSize * sizeof(float) * TexBufferConfig::SamplerElements(_ext_tb.first)));
         }
         for (const auto& _ext_tb : ext_tex_buffers) {
             auto& ext_tb = _ext_tb.second;
             for (auto int_tb = as.custom_texture_buffers.find(_ext_tb.first); int_tb != as.custom_texture_buffers.end(); ++int_tb) {
                 if (ext_tb.nameInShader == int_tb->second.first.nameInShader) {
-                    visassert(_cudaMemcpyDeviceToDevice(int_tb->second.second->d_mappedPointer, ext_tb.t_d_ptr, as.dataSize * sizeof(float)));
+                    visassert(_cudaMemcpyDeviceToDevice(int_tb->second.second->d_mappedPointer, ext_tb.t_d_ptr, as.dataSize * sizeof(float) * TexBufferConfig::SamplerElements(_ext_tb.first)));
                 }
             }
         }

CMakeLists.txt
@@ -258,6 +258,7 @@ SET(VISUALISER_SRC
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/ShaderHeader.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/Shaders.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/ShadersVec.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/PositionFunction.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/DirectionFunction.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/ScaleFunction.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/texture/Texture.h
@@ -289,6 +290,7 @@ SET(VISUALISER_SRC
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/lights/LightsBuffer.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/ShaderCore.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/Shaders.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/PositionFunction.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/DirectionFunction.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/shader/ScaleFunction.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/flamegpu/visualiser/texture/Texture.cpp
@@ -567,8 +569,8 @@ else()
         ${CMAKE_CURRENT_SOURCE_DIR}/resources/stuntplane.obj
 # Tdir: These shaders are setup to receive an appended rotation function
 # Tcolor: These shaders are setup to receive an appended color function
-        ${CMAKE_CURRENT_SOURCE_DIR}/resources/instanced_default_Tdir_Tscale.vert
-        ${CMAKE_CURRENT_SOURCE_DIR}/resources/instanced_default_Tcolor_Tdir_Tscale.vert
+        ${CMAKE_CURRENT_SOURCE_DIR}/resources/instanced_default_Tpos_Tdir_Tscale.vert
+        ${CMAKE_CURRENT_SOURCE_DIR}/resources/instanced_default_Tcolor_Tpos_Tdir_Tscale.vert
         ${CMAKE_CURRENT_SOURCE_DIR}/resources/material_flat_Tcolor.frag
     )
     cmrc_add_resource_library(resources ${RESOURCES_ALL})

include/flamegpu/visualiser/config/TexBufferConfig.h
@@ -16,6 +16,8 @@ struct TexBufferConfig {
          * Agent location x/y/z
          */
         Position_x, Position_y, Position_z,
+        Position_xy,
+        Position_xyz,
         /**
          * Agent forward/up direction x/y/z
          */
@@ -46,9 +48,11 @@ struct TexBufferConfig {
     };
     static std::string SamplerName(Function f) {
         switch (f) {
-        case Position_x: return "x_pos";
-        case Position_y: return "y_pos";
-        case Position_z: return "z_pos";
+        case Position_x: return "_pos_x";
+        case Position_y: return "_pos_y";
+        case Position_z: return "_pos_z";
+        case Position_xy: return "_pos_xy";
+        case Position_xyz: return "_pos_xyz";
         case Forward_x: return "_fw_x";
         case Forward_y: return "_fw_y";
         case Forward_z: return "_fw_z";
@@ -66,6 +70,34 @@ struct TexBufferConfig {
         case Color:
         case Unknown:
         default: return "";
+        }
+    }
+    static int SamplerElements(Function f) {
+        switch (f) {
+        case Position_xyz:
+            return 3;
+        case Position_xy:
+            return 2;
+        case Position_x:
+        case Position_y:
+        case Position_z:
+        case Forward_x:
+        case Forward_y:
+        case Forward_z:
+        case Up_x:
+        case Up_y:
+        case Up_z:
+        case Heading:
+        case Pitch:
+        case Bank:
+        case Scale_x:
+        case Scale_y:
+        case Scale_z:
+        case UniformScale:
+        case Color:
+        case Unknown:
+        default:
+            return 1;
         }
     }
     explicit TexBufferConfig(const std::string& _agentVariableName = "")

resources/instanced_default_Tcolor_Tpos_Tdir_Tscale.vert
@@ -9,10 +9,6 @@ in vec3 _vertex;
 in vec3 _normal;
 in vec2 _texCoords;
 
-uniform samplerBuffer x_pos;
-uniform samplerBuffer y_pos;
-uniform samplerBuffer z_pos;
-
 out vec3 eyeVertex;
 out vec3 eyeNormal;
 out vec2 texCoords;
@@ -21,6 +17,7 @@ flat out int shaderColor;
 
 vec3 getScale();
 mat3 getDirection();
+vec3 getPosition();
 vec4 calculateColor();
 void main() {
   // Apply model matrix to raw vertex
@@ -30,15 +27,14 @@ void main() {
   // Apply a user defined rotation
   mat3 directionMat = getDirection();
   vert.xyz = directionMat * vert.xyz;
-  //Grab model offset from texture array
-  vec3 loc_data = vec3(texelFetch(x_pos, gl_InstanceID).x, texelFetch(y_pos, gl_InstanceID).x, texelFetch(z_pos, gl_InstanceID).x);
+  // Grab model offset from texture array
+  vec3 loc_data = getPosition();
   // Apply loc_data translation to vert
   vert.xyz += loc_data;
   // Calculate eye vertex
   eyeVertex = (_viewMat * vert).rgb;
   // Calculate frag vertex
   gl_Position = _projectionMat * vec4(eyeVertex, 1.0f);
-  
   
   // Calc eye normal
   eyeNormal = normalize(_normalMat * transpose(inverse(directionMat)) * normalize(_normal));
@@ -50,4 +46,3 @@ void main() {
   // Likely more performant to include this directly in the frag shader so branches can be optimised out
   shaderColor = 1;
 }
-

resources/instanced_default_Tpos_Tdir_Tscale.vert
@@ -9,16 +9,13 @@ in vec3 _vertex;
 in vec3 _normal;
 in vec2 _texCoords;
 
-uniform samplerBuffer x_pos;
-uniform samplerBuffer y_pos;
-uniform samplerBuffer z_pos;
-
 out vec3 eyeVertex;
 out vec3 eyeNormal;
 out vec2 texCoords;
 
 vec3 getScale();
 mat3 getDirection();
+vec3 getPosition();
 void main()
 {
   // Apply model matrix to raw vertex
@@ -28,15 +25,14 @@ void main()
   // Apply a user defined rotation
   mat3 directionMat = getDirection();
   vert.xyz = directionMat * vert.xyz;
-  //Grab model offset from texture array
-  vec3 loc_data = vec3(texelFetch(x_pos, gl_InstanceID).x, texelFetch(y_pos, gl_InstanceID).x, texelFetch(z_pos, gl_InstanceID).x);
+  // Grab model offset from texture array
+  vec3 loc_data = getPosition();
   // Apply loc_data translation to vert
   vert.xyz += loc_data;
   // Calculate eye vertex
   eyeVertex = (_viewMat * vert).rgb;
   // Calculate frag vertex
   gl_Position = _projectionMat * vec4(eyeVertex, 1.0f);
-  
   
   // Calc eye normal
   eyeNormal = normalize(_normalMat * transpose(inverse(directionMat)) * normalize(_normal));

src/flamegpu/visualiser/shader/PositionFunction.cpp
@@ -1,0 +1,53 @@
+#include "flamegpu/visualiser/shader/PositionFunction.h"
+
+#include <sstream>
+
+
+namespace flamegpu {
+namespace visualiser {
+
+    PositionFunction::PositionFunction(const std::map<TexBufferConfig::Function, TexBufferConfig> &tex_buffers)
+    : has_pos_x(tex_buffers.find(TexBufferConfig::Position_x) != tex_buffers.end())
+    , has_pos_y(tex_buffers.find(TexBufferConfig::Position_y) != tex_buffers.end())
+    , has_pos_z(tex_buffers.find(TexBufferConfig::Position_z) != tex_buffers.end())
+    , has_pos_xy(tex_buffers.find(TexBufferConfig::Position_xy) != tex_buffers.end())
+    , has_pos_xyz(tex_buffers.find(TexBufferConfig::Position_xyz) != tex_buffers.end())
+{ }
+
+std::string PositionFunction::getSrc() {
+    std::stringstream ss;
+    // Define any sampler buffers
+    if (has_pos_x) ss << "uniform samplerBuffer _pos_x;" << "\n";
+    if (has_pos_y) ss << "uniform samplerBuffer _pos_y;" << "\n";
+    if (has_pos_z) ss << "uniform samplerBuffer _pos_z;" << "\n";
+    if (has_pos_xy) ss << "uniform samplerBuffer _pos_xy;" << "\n";
+    if (has_pos_xyz) ss << "uniform samplerBuffer _pos_xyz;" << "\n";
+    // Begin function
+    ss << "vec3 getPosition() {" << "\n";
+    if (has_pos_x || has_pos_y || has_pos_z) {
+        ss << "    return vec3(" <<"\n";
+        ss << "        " << (has_pos_x ? "texelFetch(_pos_x, gl_InstanceID).x" : "0") << "," << "\n";
+        ss << "        " << (has_pos_y ? "texelFetch(_pos_y, gl_InstanceID).x" : "0") << "," << "\n";
+        ss << "        " << (has_pos_z ? "texelFetch(_pos_z, gl_InstanceID).x" : "0") << ");" << "\n";
+    } else if (has_pos_xy) {
+        ss << "    const int t = gl_InstanceID * 2;" << "\n";
+        ss << "    return vec3(" << "\n";
+        ss << "        texelFetch(_pos_xy, t).x," << "\n";
+        ss << "        texelFetch(_pos_xy, t + 1).x," << "\n";
+        ss << "        0);" << "\n";
+    } else if (has_pos_xyz) {
+        ss << "    const int t = gl_InstanceID * 3;" << "\n";
+        ss << "    return vec3(" << "\n";
+        ss << "        texelFetch(_pos_xyz, t).x," << "\n";
+        ss << "        texelFetch(_pos_xyz, t + 1).x," << "\n";
+        ss << "        texelFetch(_pos_xyz, t + 2).x);" << "\n";
+    } else {
+        ss << "return glm::vec3(0);" << "\n";
+    }
+    ss << "}" << "\n";
+
+    return ss.str();
+}
+
+}  // namespace visualiser
+}  // namespace flamegpu

src/flamegpu/visualiser/shader/PositionFunction.h
@@ -1,0 +1,37 @@
+#ifndef SRC_FLAMEGPU_VISUALISER_SHADER_POSITIONFUNCTION_H_
+#define SRC_FLAMEGPU_VISUALISER_SHADER_POSITIONFUNCTION_H_
+
+#include <string>
+#include <map>
+
+#include "flamegpu/visualiser/config/TexBufferConfig.h"
+
+
+namespace flamegpu {
+namespace visualiser {
+
+/**
+ * Produces the source for a GLSL function which returns a position vector
+ * The function will have the following prototype
+ * vec3 getPosition()
+ */
+class PositionFunction {
+ public:
+    explicit PositionFunction(const std::map<TexBufferConfig::Function, TexBufferConfig> &tex_buffers);
+    /**
+     * Returns the glsl function mat3 getPosition()
+     */
+    std::string getSrc();
+
+ private:
+    bool has_pos_x;
+    bool has_pos_y;
+    bool has_pos_z;
+    bool has_pos_xy;
+    bool has_pos_xyz;
+};
+
+}  // namespace visualiser
+}  // namespace flamegpu
+
+#endif  // SRC_FLAMEGPU_VISUALISER_SHADER_POSITIONFUNCTION_H_

commit cf03f1c76263f5daab36ba0d21d014133e2b860a

BugFix: Crash when population with xyz position grows.
This bug was introduced by 3966b66.

When copying data to tex buffers, the buffer len (of the first buffer, aka position), is checked against the requested agent copy count. As float3 buffers are not supported, a float1 buffer of Nx length is used to support array variables, hence the comparison assumed the buffer was Nx longer than it was.

This fix simply divides the buffer len by that multiplier.

src/flamegpu/visualiser/Visualiser.cpp
@@ -576,8 +576,9 @@ void Visualiser::updateAgentStateBuffer(const std::string &agent_name, const std
     if (as.core_texture_buffers.empty() || buffLen == 0)
         return;
     //  Copy Data
-    const auto& first_buff = as.core_texture_buffers.begin()->second;
-    const unsigned int buff_size = first_buff ? first_buff->elementCount : 0;
+    const auto& first = as.core_texture_buffers.begin();
+    const auto& first_buff = first->second;
+    const unsigned int buff_size = first_buff ? first_buff->elementCount / TexBufferConfig::SamplerElements(first->first) : 0;
     if (buff_size) {
         // If buffer has to resize, we may not copy data for new agents
         as.dataSize = buffLen < buff_size ? buffLen : buff_size;