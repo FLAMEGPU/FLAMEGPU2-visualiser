629297f996b52c914051da5ba766d66f9fce3b5c
Peter Heywood <peethwd@gmail.com>
2025-07-28 14:56:32 +0100

    CI Updates: Update images to match currently supported images + manylinux

--- .github/scripts/install_cuda_el8.sh
+++ .github/scripts/install_cuda_el8.sh
@@ -1,20 +1,23 @@
-# @todo - better / more robust parsing of inputs from env vars.
+# Install CUDA on Alma8/manylinux_2_28.
+
 ## -------------------
 ## Constants
 ## -------------------
 
+# dnf install cuda-nvrtc-devel-11-4 cuda-compiler-11-4 cuda-cudart-devel-11-4 cuda-nvcc-11-4 cuda-nvrtc-11-4 cuda-nvtx-11-4 libcurand-devel-11-4
+
 # List of sub-packages to install.
-# @todo - pass this in from outside the script? 
+# @todo - pass this in from outside the script?
 # @todo - check the specified subpackages exist via apt pre-install?  apt-rdepends cuda-9-0 | grep "^cuda-"?
 
-# Ideally choose from the list of meta-packages to minimise variance between cuda versions (although it does change too). Some of these packages may not be availble in older CUDA releases
+# Ideally choose from the list of meta-packages to minimise variance between cuda versions (although it does change too)
 CUDA_PACKAGES_IN=(
     "cuda-compiler"
-    "cuda-cudart-dev"
+    "cuda-cudart-devel" # libcudart.so
+    "cuda-driver-devel" # libcuda.so
     "cuda-nvtx"
-    "cuda-nvrtc-dev"
-    "libcurand-dev" # 11-0+
-    "cuda-cccl" # 11.4+, provides cub and thrust. On 11.3 knwon as cuda-thrust-11-3
+    "cuda-nvrtc-devel"
+    "libcurand-devel" # 11-0+
 )
 
 ## -------------------
@@ -41,6 +44,7 @@ function version_lt() {
     [ "$1" = "$2" ] && return 1 || version_le $1 $2
 }
 
+
 ## -------------------
 ## Select CUDA version
 ## -------------------
@@ -53,15 +57,13 @@ CUDA_VERSION_MAJOR_MINOR=${cuda}
 CUDA_MAJOR=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f1)
 CUDA_MINOR=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f2)
 CUDA_PATCH=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f3)
-# use lsb_release to find the OS.
-UBUNTU_VERSION=$(lsb_release -sr)
-UBUNTU_VERSION="${UBUNTU_VERSION//.}"
+# query rpm to find the major enterprise linux release
+EL_MAJOR=$(rpm -E %{rhel})
 
 echo "CUDA_MAJOR: ${CUDA_MAJOR}"
 echo "CUDA_MINOR: ${CUDA_MINOR}"
 echo "CUDA_PATCH: ${CUDA_PATCH}"
-# echo "UBUNTU_NAME: ${UBUNTU_NAME}"
-echo "UBUNTU_VERSION: ${UBUNTU_VERSION}"
+echo "EL_MAJOR:   ${EL_MAJOR}"
 
 # If we don't know the CUDA_MAJOR or MINOR, error.
 if [ -z "${CUDA_MAJOR}" ] ; then
@@ -73,40 +75,24 @@ if [ -z "${CUDA_MINOR}" ] ; then
     exit 1
 fi
 # If we don't know the Ubuntu version, error.
-if [ -z ${UBUNTU_VERSION} ]; then
-    echo "Error: Unknown Ubuntu version. Aborting."
+if [ -z ${EL_MAJOR} ]; then
+    echo "Error: Unknown EL version. Aborting."
     exit 1
 fi
-
 
 ## -------------------------------
 ## Select CUDA packages to install
 ## -------------------------------
 CUDA_PACKAGES=""
 for package in "${CUDA_PACKAGES_IN[@]}"
-do : 
-    # @todo This is not perfect. Should probably provide a separate list for diff versions
-    # cuda-compiler-X-Y if CUDA >= 9.1 else cuda-nvcc-X-Y
-    if [[ "${package}" == "cuda-nvcc" ]] && version_ge "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
-        package="cuda-compiler"
-    elif [[ "${package}" == "cuda-compiler" ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
-        package="cuda-nvcc"
-    # CUB/Thrust  are packages in cuda-thrust in 11.3, but cuda-cccl in 11.4+
-    elif [[ "${package}" == "cuda-thrust" || "${package}" == "cuda-cccl" ]]; then
-        # CUDA cuda-thrust >= 11.4
-        if version_ge "$CUDA_VERSION_MAJOR_MINOR" "11.4" ; then
-            package="cuda-cccl"
-        # Use cuda-thrust > 11.2
-        elif version_ge "$CUDA_VERSION_MAJOR_MINOR" "11.3" ; then
-            package="cuda-thrust"
-        # Do not include this pacakge < 11.3
-        else
-            continue
-        fi
-    fi
-    # CUDA 11+ includes lib* / lib*-dev packages, which if they existed previously where cuda-cu*- / cuda-cu*-dev-
+do :
+    # CUDA < 11, lib* packages were actually cuda-cu* (generally, this might be greedy.)
     if [[ ${package} == libcu* ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "11.0" ; then
         package="${package/libcu/cuda-cu}"
+    fi
+    # CUDA < 11, -devel- packages were actually -dev
+    if [[ ${package} == *devel* ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "11.0" ; then
+        package="${package//devel/dev}"
     fi
     # Build the full package name and append to the string.
     CUDA_PACKAGES+=" ${package}-${CUDA_MAJOR}-${CUDA_MINOR}"
@@ -116,18 +102,13 @@ echo "CUDA_PACKAGES ${CUDA_PACKAGES}"
 ## -----------------
 ## Prepare to install
 ## -----------------
-CPU_ARCH="x86_64"
-PIN_FILENAME="cuda-ubuntu${UBUNTU_VERSION}.pin"
-PIN_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/${PIN_FILENAME}"
-# apt keyring package now available https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
-KERYRING_PACKAGE_FILENAME="cuda-keyring_1.1-1_all.deb"
-KEYRING_PACKAGE_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/${KERYRING_PACKAGE_FILENAME}"
-REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/"
 
-echo "PIN_FILENAME ${PIN_FILENAME}"
-echo "PIN_URL ${PIN_URL}"
-echo "KEYRING_PACKAGE_URL ${KEYRING_PACKAGE_URL}"
-echo "APT_KEY_URL ${APT_KEY_URL}"
+CPU_ARCH="x86_64"
+# Nvidia don't provide an explicit alma repo. 11.2's closest is RHEL.
+# 12.4 includes rocky8/9, rhel7/8/9, cent7, so RHEL is the closes that should hopefully be fine, otherwise will have to switch to the much slower runfile installer.
+DNF_REPO_URI="https://developer.download.nvidia.com/compute/cuda/repos/rhel${EL_MAJOR}/${CPU_ARCH}/cuda-rhel${EL_MAJOR}.repo"
+
+echo "DNF_REPO_URI ${DNF_REPO_URI}"
 
 ## -----------------
 ## Check for root/sudo
@@ -143,8 +124,8 @@ has_sudo=false
 if command -v sudo &> /dev/null ; then
     has_sudo=true
 fi
-# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not. 
-if [ "$is_root" = false ] && [ "$has_sudo" = false ]; then 
+# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not.
+if [ "$is_root" = false ] && [ "$has_sudo" = false ]; then
     echo "Root or sudo is required. Aborting."
     exit 1
 elif [ "$is_root" = false ] ; then
@@ -157,14 +138,11 @@ fi
 ## Install
 ## -----------------
 echo "Adding CUDA Repository"
-wget ${PIN_URL}
-$USE_SUDO mv ${PIN_FILENAME} /etc/apt/preferences.d/cuda-repository-pin-600
-wget ${KEYRING_PACKAGE_URL} && ${USE_SUDO} dpkg -i ${KERYRING_PACKAGE_FILENAME} && rm ${KERYRING_PACKAGE_FILENAME}
-$USE_SUDO add-apt-repository "deb ${REPO_URL} /"
-$USE_SUDO apt-get update
+$USE_SUDO dnf config-manager --add-repo ${DNF_REPO_URI}
+$USE_SUDO dnf clean all
 
 echo "Installing CUDA packages ${CUDA_PACKAGES}"
-$USE_SUDO apt-get -y install ${CUDA_PACKAGES}
+$USE_SUDO dnf -y install ${CUDA_PACKAGES}
 
 if [[ $? -ne 0 ]]; then
     echo "CUDA Installation Error."
@@ -183,7 +161,6 @@ export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
 # Check nvcc is now available.
 nvcc -V
-
 
 # If executed on github actions, make the appropriate echo statements to update the environment
 if [[ $GITHUB_ACTIONS ]]; then
